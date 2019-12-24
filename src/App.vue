42c0476278399751778994b8b8c3f77f41935261
RinoTsuka <rino.tsuka.1226@gmail.com>
2019-12-24 21:29:38 +0900

    scss作成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app" class="app">
     <v-hero/>
-    
   </div>
 </template>
 
@@ -13,41 +12,8 @@ export default {
 }
 </script>
 
-<style>
-@font-face {
-  font-family: "Original Yu Gothic";
-  src: local("Yu Gothic Medium");
-  font-weight: 100;
-}
-@font-face {
-  font-family: "Original Yu Gothic";
-  src: local("Yu Gothic Medium");
-  font-weight: 200;
-}
-@font-face {
-  font-family: "Original Yu Gothic";
-  src: local("Yu Gothic Medium");
-  font-weight: 300;
-}
-@font-face {
-  font-family: "Original Yu Gothic";
-  src: local("Yu Gothic Medium");
-  font-weight: 400;
-}
-@font-face {
-  font-family: "Original Yu Gothic";
-  src: local("Yu Gothic Bold");
-  font-weight: bold;
-}
-
-html {
-}
-
-body {
-  background: #2d4159 url(/img/common/background.png) center center / 100px 100px;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Hiragino Sans", "Noto Sans CJK JP", "Original Yu Gothic", "Yu Gothic", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Sans Emoji";
-  font-size: 0.875rem;
-}
+<style lang="scss">
+@import "@/assets/scss/app";
 
 .app {
   height: 100%;
