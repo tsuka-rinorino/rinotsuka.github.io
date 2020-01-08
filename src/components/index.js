a98d395c11b80308be6c54e1e5940079ec915361
RinoTsuka <rino.tsuka.1226@gmail.com>
2020-01-08 22:22:25 +0900

    docs展開

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-Vue.component('v-heading', require('./heading/index').default)
-Vue.component('v-hero', require('./hero/index').default)
-Vue.component('v-profile', require('./profile/index').default)
-Vue.component('v-work', require('./work/index').default)
-Vue.component('v-post', require('./post/index').default)
-Vue.component('v-footer', require('./footer/index').default)
-Vue.component('v-sns', require('./sns/index').default)
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './components/index'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
